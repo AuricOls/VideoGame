302a0e950483e44767b9b1b9e78322fccbe43d51
AuricOls <112778782+AuricOls@users.noreply.github.com>
2023-03-15 10:21:54 -0600

    Update game.vhd

--- game.vhd
+++ game.vhd
@@ -17,20 +17,36 @@ ENTITY game IS
 		Vd: INTEGER := 525); --Vpulse+VBP+Vactive+VFP
 	PORT (
 		clk: IN STD_LOGIC; --50MHz in our board
-		update_mode:in std_logic:='1'; --button to update the image
-		sw_selector: IN STD_LOGIC_vector(1 downto 0);
+		update_mode:in std_logic; --button to update the image
+		sw_selector: IN STD_LOGIC_vector(9 downto 0);
 		pixel_clk: BUFFER STD_LOGIC;
 		Hsync, Vsync: BUFFER STD_LOGIC;
 		R, G, B: OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
-		nblanck, nsync : OUT STD_LOGIC);
-	END game;
+		led_bcd: out 		std_logic_vector( 6 downto 0 );
+		nblanck, nsync : OUT STD_LOGIC
+		);
+END game;
+	
+
 
 ARCHITECTURE logic OF game IS
 SIGNAL Hactive, Vactive, dena: STD_LOGIC;
 constant counter_limit  : natural := 50000000;
+signal button_sim: std_logic_vector(1 downto 0);
+
+signal output_1a : std_logic_vector(3 downto 0);
+signal output_1b : std_logic_vector(3 downto 0);
+
+component de10lite IS
+	PORT(	
+		CLOCK_50	: 	IN			std_logic;
+		KEY		: 	IN 		std_logic_vector( 1 DOWNTO 0 );
+		SW			: 	IN 		std_logic_vector( 9 DOWNTO 0 );
+		hex0     : 	out 		std_logic_vector( 6 downto 0 )
+	);
+END component de10lite;
 
 begin
-
 
 -------------------------------------------------------
 --Part 1: CONTROL GENERATOR
@@ -73,6 +89,7 @@ begin
 	-------------------------------------------------------
 	--Part 2: IMAGE GENERATOR
 	-------------------------------------------------------
+
 	PROCESS (Hsync, Vsync,Hactive, Vactive, dena)
 	
 	VARIABLE line_counter: INTEGER RANGE 0 TO Vc;
@@ -83,9 +100,13 @@ begin
 	variable ship_position_y:integer range 0 to Vc:=270;
 	variable obstacle_pos_x:integer range 0 to Hc:=700;
 	variable obstacle_pos_y:integer range 0 to Vc:=480;
-	variable counter:integer range 0 to 700000;
-	variable counter_time:integer range 0 to 25;
-
+	variable counter:integer range 0 to 500000;
+	variable counter_time:integer range 0 to 30;
+	variable y_collision_obstacle:integer range 0 to Vc:=230;
+	--collision in x is obstacle_pos_x-150
+	
+	
+					  
 	
 	BEGIN
 		
@@ -108,8 +129,11 @@ begin
 		
 		IF (dena='1') THEN  --(x,y,h,w)
 			
-	
-			iF ((line_counter>= (ship_position_y-70) and line_counter<ship_position_y) and (column_counter >= (ship_position_x-50) and column_counter <= ship_position_x)) THEN
+		if((line_counter<=obstacle_pos_y and (line_counter>obstacle_pos_y-250)) and (column_counter>(obstacle_pos_x-150) and column_counter<=obstacle_pos_x)) then
+					R <= (OTHERS => '1');
+					G <= (OTHERS => '0');
+					B <= (OTHERS => '0');
+			elsiF ((line_counter>= (ship_position_y-70) and line_counter<ship_position_y) and (column_counter >= (ship_position_x-50) and column_counter <= ship_position_x)) THEN
 					R <= "1111";
 					G <="0101";
 					B <="1111";
@@ -124,21 +148,14 @@ begin
 					B <= (OTHERS => '0');
 			
 				end if;
-			elsif((line_counter<=obstacle_pos_y and (line_counter>obstacle_pos_y-250)) and (column_counter>(obstacle_pos_x-150) and column_counter<=obstacle_pos_x)) then
-				R <= (OTHERS => '1');
-				G <= (OTHERS => '0');
-				B <= (OTHERS => '0');
-	 
+		
 			else
 				R <= (OTHERS => '0');
 				G <= (OTHERS => '0');
 				B <= (OTHERS => '0');
 			end if;
-			
-			
-			
-		
---					
+	
+					
 		ELSE
 			R <= (OTHERS => '0');
 			G <= (OTHERS => '0');
@@ -151,50 +168,30 @@ begin
 --			--11 go up, 00 go down, 01 go left, 10 go right
 --
 				if (rising_edge(clk)) then
+					--move obstacle
 					if (counter=15000) then
 						obstacle_pos_x:=obstacle_pos_x-5;
 						end if;
+
+
 					if (update_mode='1') then
 						counter_time:=0;
 						ship_position_y:=starting_position_y;
-					elsif (update_mode='0' and counter_time<20) then
+					elsif (update_mode='0' and counter_time<=25) then
 						ship_position_y:=ship_position_y-2;
 						counter_time:=counter_time+1;
 					end if;
 					
-----					case sw_selector is
-----						when "11" =>
-----							if (update_mode='0' and counter_time<10) then 
-----								counter_time:=counter_time+1;
-----								up_limit:= up_limit-step;
-----								down_limit:= down_limit-step;
-----							end if;
-----							
-----						when "00" =>
---							if (update_mode='0' and counter_time=0) then 
---								counter_time:=counter_time+1;
---								up_limit:= up_limit+step;
---								down_limit:= down_limit+step;
---							end if;
---							
---						when "01" =>
---							if (update_mode='0' and counter_time=0) then 
---								counter_time:=counter_time+1;
---								left_limit:= left_limit-step;
---								right_limit:= right_limit-step;
---							end if;	
---						when "10" =>
---							if (update_mode='0' and counter_time=0) then 
---								counter_time:=counter_time+1;
---								left_limit:= left_limit+step;
---								right_limit:= right_limit+step;
---							end if;							
---						when others=>
---							null;
---							
---					end case;
-				end if;
-	
 
+					if ((ship_position_x >= (obstacle_pos_x - 150)) and (ship_position_y > y_collision_obstacle)) then
+							button_sim<="01";
+					else
+							button_sim<="00";
+
+					end if;
+				end if;
 	END PROCESS;
+	
+	death_counter:de10lite port map(clk,button_sim,sw_selector,led_bcd);
+
 END logic;
